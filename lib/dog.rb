b2571d7e97c41af4b2e6206d9337307343c712df
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-16 06:57:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Dog
  def name=(dog_name)
-   @name = dog_name
+   name = @dog_name
  end
  def name
-   puts @dog_name
+   puts dog_name
   end
   def bark
     puts "woof!"
